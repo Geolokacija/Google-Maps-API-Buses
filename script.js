144e032a01a43ba30803ec7501302e12ec5d631a
Mantas <mantassalkovskis@gmail.com>
2018-03-02 22:49:06 +0200

    Merge branch 'master' of D:\IT Programs\WebStorm Workplace\Application\Google-Maps-API-Buses with conflicts.

--- script.js
+++ script.js
@@ -2,96 +2,66 @@
 
 $.getJSON('http://api-ext.trafi.com/routes?start_lat=55.66542159999999&start_lng=21.176730799999973&end_lat=55.7284356&end_lng=21.125247100000024&is_arrival=false&api_key=b8bee4f34d5c2b7fbbcab7533638870d',
     function(data) {
-        // console.log(data);
-    });
+    // console.log(data);
+});
 
-$.getJSON('http://api-ext.trafi.com/stops/nearby?lat=55.7021303&lng=21.143823300000008&api_key=b8bee4f34d5c2b7fbbcab7533638870d',
+$.getJSON('http://api-ext.trafi.com/stops/nearby?lat=55.703297&lng=21.144279&radius=50000&api_key=01f86ef81f0a2d7414bdd0bcfd9f3adc',
     function(data) {
-        var jsObject = JSON.parse(JSON.stringify(data));
-        var dataFromServer = [];
-        for (var i = 0; i < jsObject.Stops.length; i++) {
-            dataFromServer[dataFromServer.length] = jsObject.Stops[i];
-        }
-        //sending "dataFromServer" array to function, which adds bus stops markers
-        addNearBusStops(dataFromServer);
-    });
     var jsObject = JSON.parse(JSON.stringify(data));
     var dataFromServer = [];
     for (var i = 0; i < jsObject.Stops.length; i++) {
         dataFromServer[dataFromServer.length] = jsObject.Stops[i];
     }
-    mantis(jsObject);
     //sending "dataFromServer" array to function, which adds bus stops markers
     addNearBusStops(dataFromServer);
 });
 
 $.getJSON('http://api-ext.trafi.com/locations?q=rumpiske&region=klaipeda&current_lat=55.703229&current_lng=21.148679000000016&api_key=b8bee4f34d5c2b7fbbcab7533638870d',
     function(data) {
-        // console.log(data);
-    });
+    // console.log(data);
+});
 
 $.getJSON('http://api-ext.trafi.com/departures?stop_id=idjkb_7-9%20Cawang%20UKI&region=jakarta&api_key=01f86ef81f0a2d7414bdd0bcfd9f3adc',
     function(data) {
-        // console.log(data);
-    });
+    // console.log(data);
+});
+
+
 var bus;
-var y=1;
+var map;
+
 setTimeout(function busses() {
     $.get('http://stops.lt/klaipeda/gps.txt?'+Date.now() ,
         function(data) {
-            var a = [] = data.split("\n");
-            console.log(a.length-1+ "\n"+a[12]+ "\n" + a[7])
-          /*  var s =[];
-            var person;
-            for(var i=0;i<a.length-1;i+=7){
-                s[y] = [a[i], a[i+1], a[i+2], a[i+3],a[i+4],a[i+5], a[i+6]];
-               y++;
-            }
-            y=1;
-            // for (var i=0;i<a.length;i++)
-                console.log(s);*/
+           // console.log(data);
         });
+   // console.log("Busses");
     setTimeout(function () {
         busses();
     },5000)
 
-},5000)
+},5000);
 
 
-function initMap() {
-    var centerPoint = {lat: 55.70329479999999, lng: 21.14427950000004};
-    var map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 4,
-        center: centerPoint
-    });
-}
+  function initMap() {
+          var centerPoint = {lat: 55.70329479999999, lng: 21.14427950000004};
+           map = new google.maps.Map(document.getElementById('map'), {
+            zoom: 13,
+            center: centerPoint
+          });
+  }
 
-var mantis1 = [];
-var cordX = [];
-var cordY = [];
-var abejuCordArry = [];
-function mantis(mantis)
-{
-    mantis1 = mantis;
-    for(var i =0; i < mantis.Stops.length; i++)
-    {
-        cordX[i] = mantis.Stops[i].Coordinate.Lat.toString();
-        cordY[i] = mantis.Stops[i].Coordinates.Lng.toString();
-        for()
-        {
+var markerArr = [];
 
-        }
-        //console.log(mantis.Stops[i].Coordinate.Lat.toString());
-        console.log(cord[i]);
+  function addNearBusStops(dataFromServer) {
+      for (var i = 0; i < dataFromServer.length; i++) {
+          var stopsCords = new google.maps.LatLng(dataFromServer[i].Coordinate.Lat, dataFromServer[i].Coordinate.Lng);
 
+          markerArr[i] = new google.maps.Marker({
+              position: stopsCords,
+              map: map,
+              title: "" + dataFromServer[i].Name
+          });
+      }
+  }
 
-    }
-}
-
-
-function addNearBusStops(dataFromServer) {
-
-    for (var i = 0; i < dataFromServer.length; i++) {
-        console.log(dataFromServer[i]);
-    }
-}
