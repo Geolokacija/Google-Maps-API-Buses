9771e082ae2f115c3e87ca3d2552d022e7c155a4
geolokacija <senasnaujokas@gmail.com>
2018-03-02 10:38:08 +0200

    Add empty map (Dovydas)

--- script.js
+++ script.js
@@ -14,3 +14,12 @@ $.getJSON('http://api-ext.trafi.com/locations?q=rumpiske&region=klaipeda&current
 $.getJSON('http://api-ext.trafi.com/departures?stop_id=idjkb_7-9%20Cawang%20UKI&region=jakarta&api_key=01f86ef81f0a2d7414bdd0bcfd9f3adc', function(data) {
     console.log(data);
 });
+
+function initMap() {
+        var centerPoint = {lat: 55.70329479999999, lng: 21.14427950000004};
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 4,
+          center: centerPoint
+        });
+
+      }
