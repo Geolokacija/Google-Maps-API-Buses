b63b1f588d82b7b622264b2c5a7c893d52aca983
Dziugas <dziuras123@gmail.com>
2018-03-02 11:46:11 +0200

    busses
    Signed-off-by: root <senasnaujokas@gmail.com>

--- script.js
+++ script.js
@@ -2,41 +2,42 @@
 
 $.getJSON('http://api-ext.trafi.com/routes?start_lat=55.66542159999999&start_lng=21.176730799999973&end_lat=55.7284356&end_lng=21.125247100000024&is_arrival=false&api_key=b8bee4f34d5c2b7fbbcab7533638870d',
     function(data) {
-    // console.log(data);
-});
+        // console.log(data);
+    });
 
 $.getJSON('http://api-ext.trafi.com/stops/nearby?lat=55.7021303&lng=21.143823300000008&api_key=b8bee4f34d5c2b7fbbcab7533638870d',
     function(data) {
-    var jsObject = JSON.parse(JSON.stringify(data));
-    var dataFromServer = [];
-    for (var i = 0; i < jsObject.Stops.length; i++) {
-        dataFromServer[dataFromServer.length] = jsObject.Stops[i];
-    }
-});
+        var jsObject = JSON.parse(JSON.stringify(data));
+        var dataFromServer = [];
+        for (var i = 0; i < jsObject.Stops.length; i++) {
+            dataFromServer[dataFromServer.length] = jsObject.Stops[i];
+        }
+    });
 
 $.getJSON('http://api-ext.trafi.com/locations?q=rumpiske&region=klaipeda&current_lat=55.703229&current_lng=21.148679000000016&api_key=b8bee4f34d5c2b7fbbcab7533638870d',
     function(data) {
-    // console.log(data);
-});
+        // console.log(data);
+    });
 
 $.getJSON('http://api-ext.trafi.com/departures?stop_id=idjkb_7-9%20Cawang%20UKI&region=jakarta&api_key=01f86ef81f0a2d7414bdd0bcfd9f3adc',
     function(data) {
-    // console.log(data);
-});
+        // console.log(data);
+    });
 var bus;
 
 setTimeout(function busses() {
-    var xmlHttp = new XMLHttpRequest();
+    $.get('http://stops.lt/klaipeda/gps.txt?'+Date.now(),
+        function (data) {
+        var stripe = da
+        console.log(data)
+    })
+    /*var xmlHttp = new XMLHttpRequest();
     xmlHttp.open( "GET", 'http://stops.lt/klaipeda/gps.txt?'+Date.now(), false ); // false for synchronous request
     xmlHttp.send( null );
-    bus= xmlHttp.response;
-   console.log(bus);
-        /*$.getJSON('http://stops.lt/klaipeda/gps.txt?'+Date.now() ,
-            function(data) {
+    bus= xmlHttp;
+    console.log(bus);
 
-                console.log(data);
-            });*/
-    console.log("Busses");
+    console.log("Busses");*/
     setTimeout(function () {
         busses();
     },5000)
@@ -45,9 +46,9 @@ setTimeout(function busses() {
 
 
 function initMap() {
-        var centerPoint = {lat: 55.70329479999999, lng: 21.14427950000004};
-        var map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 4,
-          center: centerPoint
-        });
+    var centerPoint = {lat: 55.70329479999999, lng: 21.14427950000004};
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 4,
+        center: centerPoint
+    });
 }
