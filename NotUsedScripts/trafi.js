9cb6a2568c1f4fbf2f8705dedd55f979f9ae8afb
Dziugas <dziuras123@gmail.com>
2018-03-08 10:13:02 +0200

    busses(Dziugas)
    Signed-off-by: root <senasnaujokas@gmail.com>

--- NotUsedScripts/trafi.js
+++ NotUsedScripts/trafi.js
@@ -12,7 +12,7 @@ $.getJSON('http://api-ext.trafi.com/stops/nearby?lat=55.703297&lng=21.144279&rad
         dataFromServer[dataFromServer.length] = jsObject.Stops[i];
     }
     //sending "dataFromServer" array to function, which adds bus stops markers
-    //addNearBusStops(dataFromServer);
+    addNearBusStops(dataFromServer);
 });
 
 $.getJSON('http://api-ext.trafi.com/locations?q=rumpiske&region=klaipeda&current_lat=55.703229&current_lng=21.148679000000016&api_key=b8bee4f34d5c2b7fbbcab7533638870d',
@@ -35,31 +35,32 @@ $.getJSON('http://api-ext.trafi.com/departures?' +
 
 
 
-  // Places all bus stop markers on the map
-// var markerArr = [];
-//   function addNearBusStops(dataFromServer)
-//   {
-//       for (var i = 0; i < dataFromServer.length; i++) {
-//           var stopsCords = new google.maps.LatLng(dataFromServer[i].Coordinate.Lat, dataFromServer[i].Coordinate.Lng);
-//
-//           markerArr[i] = new google.maps.Marker({
-//               position: stopsCords,
-//               map: map,
-//               title: ""+ dataFromServer[i].Name
-//           });
-//           addInfoWindow(markerArr[i], dataFromServer[i].Name)
-//       }
-//   }
-//     // Places markers infowindow with name of the stop
-//   function addInfoWindow(marker, message)
-//   {
-//       var infoWindow = new google.maps.InfoWindow({
-//               content: message
-//           });
-//
-//       google.maps.event.addListener(marker, 'click', function(){
-//           infoWindow.open(map, marker);
-//       });
-//   }
+  //Places all bus stop markers on the map
+var markerArr = [];
+  function addNearBusStops(dataFromServer)
+  {
+      for (var i = 0; i < dataFromServer.length; i++) {
+          var stopsCords = new google.maps.LatLng(dataFromServer[i].Coordinate.Lat, dataFromServer[i].Coordinate.Lng);
+
+          markerArr[i] = new google.maps.Marker({
+              position: stopsCords,
+              map: map,
+              icon: 'stop.png',
+              title: ""+ dataFromServer[i].Name
+          });
+          addInfoWindow(markerArr[i], dataFromServer[i].Name)
+      }
+  }
+    // Places markers infowindow with name of the stop
+  function addInfoWindow(marker, message)
+  {
+      var infoWindow = new google.maps.InfoWindow({
+              content: message
+          });
+
+      google.maps.event.addListener(marker, 'click', function(){
+          infoWindow.open(map, marker);
+      });
+  }
 
     // Makes 1 long bus string in 7 smaller information pieces
