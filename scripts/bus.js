a8aa4d01514285edaec93fdab5faea746def1b19
Dziugas <dziuras123@gmail.com>
2018-03-09 08:24:38 +0200

    busses(Dziugas)
    Signed-off-by: root <senasnaujokas@gmail.com>

--- scripts/bus.js
+++ scripts/bus.js
@@ -4,8 +4,8 @@ var autobusuData = [];
 var busMarkerArr = [];
 
 setTimeout(function busses() {
-    $.get('http://stops.lt/klaipeda/gps.txt?'+Date.now() ,
-        function(data) {
+    $.get('http://stops.lt/klaipeda/gps.txt?' + Date.now(),
+        function (data) {
 
             autobusuData = [] = data.split("\n");
 
@@ -14,29 +14,26 @@ setTimeout(function busses() {
 
     setTimeout(function () {
         busses();
-    },3000)
+    }, 3000)
 
-},3000);
+}, 3000);
 
 var b = [];
-function createsBusObjects(arrayOfBusString)
-{
+
+function createsBusObjects(arrayOfBusString) {
     var arrayOfBusStringLength = arrayOfBusString.length - 1; // Cut last array string
     var temporaryX;
     var temporaryY;
     var stringX;
     var stringY;
     var strLX;
-    for(var i = 1; i < arrayOfBusStringLength; i++)
-    {
+    for (var i = 1; i < arrayOfBusStringLength; i++) {
         b[i] = arrayOfBusString[i].split(",");
 
-         stringX = b[i][2];
-         strLX = stringX.length;
-        for(var a =0; a< strLX; a++)
-        {
-            if(a == 1)
-            {
+        stringX = b[i][2];
+        strLX = stringX.length;
+        for (var a = 0; a < strLX; a++) {
+            if (a == 1) {
                 temporaryX += stringX.charAt(a) + '.';
             } else {
                 temporaryX += stringX.charAt(a);
@@ -49,10 +46,8 @@ function createsBusObjects(arrayOfBusString)
         stringY = b[i][3];
         //console.log(b[i][3]);
         strLY = stringY.length;
-        for(var a =0; a< strLY; a++)
-        {
-            if(a == 1)
-            {
+        for (var a = 0; a < strLY; a++) {
+            if (a == 1) {
                 temporaryY += stringY.charAt(a) + '.';
             } else {
                 temporaryY += stringY.charAt(a);
@@ -75,16 +70,22 @@ function addMarkersAsBuses(busArray) {
         if (busMarkerArr[i] != null) {
             busMarkerArr[i].setMap(null);
         }
+        var icon = { url: "https://www.airportbus-muenchen.de/assets/img/bus-station-icon.svg",
+            scaledSize: new google.maps.Size(25, 25),
+            iconAnchor: new google.maps.Point(0.5, 0.5),
 
+
+        };
+        icon.css({
+            'transform': 'rotate(' + parseInt(busArray[i][5]) + 'deg)'});
         busMarkerArr[i] = new google.maps.Marker({
             position: busCoordinates,
             map: map,
-            title: ""+busArray[i][1],
-            icon: {
-            url: "https://www.airportbus-muenchen.de/assets/img/bus-station-icon.svg",
-            scaledSize: new google.maps.Size(25, 25)
-          }
+            title: "" + busArray[i][1],
+            icon: icon
         });
+
+        // busMarkerArr[i].style.transform="rotate("+ parseInt(busArray[i][5]) +"deg)"
 
     }
 
