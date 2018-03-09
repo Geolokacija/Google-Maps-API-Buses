115733567a23620bf6b6abd6a27cd90f31bb7b7b
Dziugas <dziuras123@gmail.com>
2018-03-09 08:39:57 +0200

    busses(Dziugas)
    Signed-off-by: root <senasnaujokas@gmail.com>

--- scripts/bus.js
+++ scripts/bus.js
@@ -61,6 +61,20 @@ function createsBusObjects(arrayOfBusString) {
 }
 
 
+function rotateIMG(angle) {
+    $('img[src="https://www.airportbus-muenchen.de/assets/img/bus-station-icon.svg"]').css({
+        'transform': 'rotate(' + angle + 'deg)'
+    });
+    google.maps.event.addListenerOnce(map, 'idle', function() {
+        setInterval(function() {
+            $('img[src="https://www.airportbus-muenchen.de/assets/img/bus-station-icon.svg"]').css({
+                'transform': 'rotate(' + 25 + 'deg)'
+            });
+            // rotationAngle += 10;
+        }, 1000);
+    });
+}
+
 function addMarkersAsBuses(busArray) {
 
     for (var i = 2; i < busArray.length; i++) {
@@ -70,14 +84,16 @@ function addMarkersAsBuses(busArray) {
         if (busMarkerArr[i] != null) {
             busMarkerArr[i].setMap(null);
         }
-        var icon = { url: "https://www.airportbus-muenchen.de/assets/img/bus-station-icon.svg",
+        var icon = {
+            url: "scripts/bus-station-icon.svg",
             scaledSize: new google.maps.Size(25, 25),
             iconAnchor: new google.maps.Point(0.5, 0.5),
-
-
         };
-        icon.css({
-            'transform': 'rotate(' + parseInt(busArray[i][5]) + 'deg)'});
+        // var icon = $('marker').css({
+        //     'transform': 'rotate(' + parseInt(busArray[i][5]) + 'deg)'});
+        rotateIMG(parseInt(busArray[i][5]));
+        //icon.scaledSize = new google.maps.Size(25, 25);
+
         busMarkerArr[i] = new google.maps.Marker({
             position: busCoordinates,
             map: map,
