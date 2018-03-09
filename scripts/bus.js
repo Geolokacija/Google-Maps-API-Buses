e96e45ff372f972e918f2016173c527edd2fe7d6
Dziugas <dziuras123@gmail.com>
2018-03-09 10:52:52 +0200

    busses(Dziugas)
    Signed-off-by: root <senasnaujokas@gmail.com>

--- scripts/bus.js
+++ scripts/bus.js
@@ -14,9 +14,9 @@ setTimeout(function busses() {
 
     setTimeout(function () {
         busses();
-    }, 3000)
+    }, 5000)
 
-}, 3000);
+}, 5000);
 
 var b = [];
 
@@ -61,20 +61,6 @@ function createsBusObjects(arrayOfBusString) {
 }
 
 
-function rotateIMG(angle) {
-    $('img[src="https://www.airportbus-muenchen.de/assets/img/bus-station-icon.svg"]').css({
-        'transform': 'rotate(' + angle + 'deg)'
-    });
-    google.maps.event.addListenerOnce(map, 'idle', function() {
-        setInterval(function() {
-            $('img[src="https://www.airportbus-muenchen.de/assets/img/bus-station-icon.svg"]').css({
-                'transform': 'rotate(' + 25 + 'deg)'
-            });
-            // rotationAngle += 10;
-        }, 1000);
-    });
-}
-
 function addMarkersAsBuses(busArray) {
 
     for (var i = 2; i < busArray.length; i++) {
@@ -84,24 +70,18 @@ function addMarkersAsBuses(busArray) {
         if (busMarkerArr[i] != null) {
             busMarkerArr[i].setMap(null);
         }
-        var icon = {
-            url: "scripts/bus-station-icon.svg",
-            scaledSize: new google.maps.Size(25, 25),
-            iconAnchor: new google.maps.Point(0.5, 0.5),
-        };
-        // var icon = $('marker').css({
-        //     'transform': 'rotate(' + parseInt(busArray[i][5]) + 'deg)'});
-        rotateIMG(parseInt(busArray[i][5]));
-        //icon.scaledSize = new google.maps.Size(25, 25);
 
         busMarkerArr[i] = new google.maps.Marker({
             position: busCoordinates,
             map: map,
             title: "" + busArray[i][1],
-            icon: icon
+            icon: {
+                path: google.maps.SymbolPath.FORWARD_CLOSED_ARROW,
+                scale: 4,
+                rotation: parseInt(busArray[i][5])
+            },
         });
 
-        // busMarkerArr[i].style.transform="rotate("+ parseInt(busArray[i][5]) +"deg)"
 
     }
 
